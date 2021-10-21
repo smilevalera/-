96dd7c668b66f6ccb60d5932bbfed411c3ecefa7
smilevalera <91686121+smilevalera@users.noreply.github.com>
2021-10-21 17:05:44 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,11 +1,12 @@
+/*валера гилёв п43021*/
 #include <iostream>
 using namespace std;
 int main() {
-	setlocale (LC_ALL, "Russian");
+	
 	int a, b;
 	cin >> a >> b;
-	if (a == 20 && b == 42)cout << "принят\n";
-	else if (a != 20 && b != 42)cout << "не принят\n";
+	if (a == 20 && b == 42)cout << "priniat\n";
+	else if (a != 20 && b != 42)cout << "ne priniat\n";
 
 
 
